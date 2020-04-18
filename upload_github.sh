6e322b396cade6542e791fa8ab8dd109b1975cf3
SMAdadey <smadadey@st.ug.edu.gh>
2020-04-18 14:38:16 +0200

    uploading_files_to_git_hub

---
+++ upload_github.sh
@@ -0,0 +1,18 @@
+
+#!/usr/bin/env bash
+
+##Uploading data to github
+
+file=$1
+msg=$2
+
+if [ $# == 2 ]; then
+        for i in $1; do git add $i; git commit -m $2; git push -u origin master; done
+else
+        echo " "
+        echo "File to upload, message about changes made"
+        echo "./upload_github.sh <input file> <message about changes made>"
+        echo " "
+fi
+
+
